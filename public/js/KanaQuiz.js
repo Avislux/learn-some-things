6b51e773cf6802948faf29ff7ea1e331a789de86
Avislux <gameguy94@hotmail.com>
2017-06-12 02:20:34 -0700

    quiz is working need diacritics

--- public/js/KanaQuiz.js
+++ public/js/KanaQuiz.js
@@ -1,31 +1,85 @@
 var userInput = $('#user-input');
 console.log("KanaQuiz.js Loaded");
-var vowels = ['a','i','u','e','o'];
-var kRow = ['ka','ki','ku','ke','ko'];
-var gRow = ['ga','gi','gu','ge','go'];
-var sRow = ['sa','shi','su','se','so'];
-var zRow = ['za','ji','zu','ze','zo'];
-var tRows = ['ta','chi','tsu','te','to'];
-var romanjiSet = ['a','i','u','e','o'];
+var vowels = ['a', 'i', 'u', 'e', 'o'];
+var kRow = ['ka', 'ki', 'ku', 'ke', 'ko'];
+var gRow = ['ga', 'gi', 'gu', 'ge', 'go'];
+var sRow = ['sa', 'shi', 'su', 'se', 'so'];
+var zRow = ['za', 'ji', 'zu', 'ze', 'zo'];
+var tRow = ['ta', 'chi', 'tsu', 'te', 'to'];
+var dRow = ['da', 'de', 'do'];
+var dRowFull = ['da', 'di', 'du', 'de', 'do'];
+var nRow = ['na', 'ni', 'nu', 'ne', 'no'];
+var hRow = ['ha', 'hi', 'fu', 'he', 'ho'];
+var bRow = ['ba', 'bi', 'bu', 'be', 'bo'];
+var pRow = ['pa', 'pi', 'pu', 'pe', 'po'];
+var mRow = ['ma', 'mi', 'mu', 'me', 'mo'];
+var yRow = ['ya',  'yu',  'yo'];
+var rRow = ['ra', 'ri', 'ru', 're', 'ro'];
+var wRow = ['wa',  'wo'];
+var otherRow = ['n'];
 var keyIndex = 0;
-console.log("From KanaQuiz:" + selectedRows); /*Imported from Quiz.pug template*/
+/*TODO: Fill json file for diacritics*/
+/*Imported from Quiz.pug template*/
+console.log("From KanaQuiz:" + selectedRows);
 
+var selectedRowsParse = selectedRows.split(",");
+var selectedKana = [];
+console.log('Selected Rows Parse');
+console.log(selectedRowsParse);
+console.log(selectedRowsParse.length);
+/*for (var row = 0; row < selectedRowsParse.length; row++){
+ console.log(row);
+ selectedKana.push.apply(selectedKana,eval(selectedRowsParse[row]));
+ }*/
+/*Equivalent with for loop.*/
+selectedRowsParse.forEach(function (row) {
+    selectedKana.push.apply(selectedKana, eval(row));
+    /*Parses string into a var name.*/
+});
+console.log('Selected kana ');
+console.log(selectedKana);
+selectedKana = shuffle(selectedKana);
+console.log(selectedKana);
+var maxKeyIndex = selectedKana.length - 1;
+var messageTextSelect = $('#message');
 /*For loop will not work*/
 $.getJSON("/resources/gujuon.json", function (json) {
-    console.log(json.a[0]);
-    /* console.log(key + " -> " + json[key]);*/
-    $('#show-kana').text(json[romanjiSet[keyIndex]][0]);
+    $('#show-kana').text(json[selectedKana[keyIndex]][0]);
+    console.log(selectedKana[keyIndex]);
+    /*0 for hiragana.*/
     userInput.keyup(function () {
-        if ($(this).val() === romanjiSet[keyIndex]) {
-            $('#message').text("Fucking lit");
+        if ($(this).val() === selectedKana[keyIndex]) {
+            messageTextSelect.text("Fucking lit");
             console.log("Got it");
-            userInput.val('');/*Clear the field*/
-            keyIndex++;
-            $('#show-kana').text(json[romanjiSet[keyIndex]][0]);
+            /*Clear the field*/
+            userInput.val('');
+
+            if (keyIndex === maxKeyIndex) {
+                messageTextSelect.text("You're done! Go away");
+            } else {
+                keyIndex++;
+            }
+            $('#show-kana').text(json[selectedKana[keyIndex]][0]);
         }
     })
 });
+function shuffle(array) {
+    var i = array.length,
+        j = 0,
+        temp;
 
-function getAnotherKana() {
+    while (i--) {
 
- }
+        j = Math.floor(Math.random() * (i + 1));
+
+        // swap randomly chosen element with current element
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+    return array;
+}
+
+
