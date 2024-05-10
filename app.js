94a096f7cbfc4e5713e9f30af95febe77f0dd35d
Avislux <gameguy94@hotmail.com>
2024-05-09 19:23:49 -0700

    Fix deploy

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var app = express();
 var kana = require('japanese-kana');
 var jsdom = require('jsdom');
 var popper = require('popper');
-var $ = jQuery = require('jquery');
 const { JSDOM } = jsdom;
 
 const { document } = (new JSDOM('')).window;
